bf80731b2f6df08f029b5620151d28842a1dff86
Kenjy Demeester <kenjy.dem@gmail.com>
2017-06-12 14:44:19 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='SLP.py',
+    version='0.1',
+    author='Kenjy Demeester',
+    author_email='kenjy.demeester@polymtl.com',
+    packages=['SLP.py'],
+    scripts=[],
+    description='Implementation of sequential linear programming using CyLP.',
+    long_description=open('README.txt').read(),
+    install_requires=[  "nlpy >= 0.2",
+                        "pyopt >= 1.0",],)
